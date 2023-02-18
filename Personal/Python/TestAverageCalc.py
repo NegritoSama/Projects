68505da9c8f1e3456353c2a1421c87d501d7b0fd
NegritoSama <101664272+NegritoSama@users.noreply.github.com>
2023-02-18 11:35:09 -0600

    Add files via upload

---
+++ Personal/Python/TestAverageCalc.py
@@ -0,0 +1,23 @@
+# Empty list to archive users scores/potential scores
+earnedPoints = []
+potentialScore = []
+num = 1
+
+# Gets how many test the user took
+totalTest = int(input("How many test did you take?: "))
+
+# Collects the score earned and potential score from user
+for i in range(totalTest):
+    pointsEarned = int(input(f"\nPlease enter points earned in test #{num}: "))
+    potentialPoints = int(input(f"Please enter total potential points of test #{num}: "))
+    potentialScore.append(potentialPoints)
+    earnedPoints.append(pointsEarned)
+    num += 1
+
+# Calculates what the average score is from the user tests
+totalEarned = sum(earnedPoints)
+totalPotential = sum(potentialScore)
+grade = totalEarned / totalPotential * 100
+
+# Prints out the average of the user's test scores
+print(f"\nYour average grade for {totalTest} tests is {round(grade)}")
