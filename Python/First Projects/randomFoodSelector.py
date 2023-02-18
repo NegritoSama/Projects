bd48017de0e20334134ce89837f15d0736a282b4
NegritoSama <101664272+NegritoSama@users.noreply.github.com>
2023-02-18 10:29:52 -0600

    First Python Mini Projects

---
+++ Python/First Projects/randomFoodSelector.py
@@ -0,0 +1,18 @@
+import random
+
+# List of different type of foods
+num = 1
+foodType = []
+numFood = int(input("Please list how many places/food you can't pick from: "))
+
+print("\nThis program will pick a restraunt or food at random since your indecisive ass can't decide")
+
+# Gets the restaurant or type of food the user can't choose from
+for i in range(numFood):
+    listFood = (input(f"\nRestaurant/Food #{num}: "))
+    foodType.append(listFood)
+    num += 1
+
+# Prints out what the user is going to eat
+randomChooser = random.choices(foodType, k=1)
+print(f"\nThe restraunt/type of food you ARE going to eat is {randomChooser}. Enjoy :D")
